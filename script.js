3149c251e1bdb998ee978c92ea730c34447b7cee
Himanshu10071 <himaanschouhan16@gmail.com>
2024-11-29 00:04:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-console.log("himanshu");
-<script>
-  function handleCredentialResponse(response) {
-    // Decode the ID token to get user info (if needed)
-    console.log("Encoded JWT ID token: " + response.credential);
-    
-    // You can use the ID token to authenticate the user on your backend
-    // or fetch user information from Google's APIs.
-  }
-</script>
+
